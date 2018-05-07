100219d2124c28b1489083084c4d14a7f29c04cd
Saranya-jena <saranyajena29@gmail.com>
2018-05-07 13:40:49 +0530

    Add files via upload

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log (name);
+		Application.LoadLevel (name);
+	}
+	public void QuitRequest(){
+		Debug.Log ("i want to quit");
+		Application.Quit ();
+	}
+
+
+}
